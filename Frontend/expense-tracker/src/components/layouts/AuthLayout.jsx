1ae6d3ebcb803408d3d8861b47b1895b0dbdcdd1
Shubham Yadav <shubham29768@gmail.com>
2025-07-09 16:35:44 +0530

    Image inserted

--- Frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ Frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -11,8 +11,13 @@ const AuthLayout = ({children}) => {
       <div className="hidden md:block w-[40vw] h-screen bg-voilet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hodden p-8 relative">
         <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"/>
         <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10"/>
-        <div className=""/>
-
+        <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5"/>
+        
+        
+        <img
+        src={CARD_2}
+        className="w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-50"
+        />
         
         </div>
 
--- Frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ Frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout'
-import myImage from '../../assets/image/card2.png';
 
 const Login = () => {
   return (
     <AuthLayout>
       <div className="w-full h-3/4 flex flex-col justify-center px-6 md:px-10">
-        <img src={myImage} alt="Card" className="mb-6 w-32 mx-auto" />
         <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
         <p className='text-xl text-slate-700 mt-[5px] mb-6'>
           Please enter your details to log in
