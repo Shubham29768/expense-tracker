01a01af4793ab5593c4a1c747d25705851d4225b
Shubham Yadav <shubham29768@gmail.com>
2025-08-01 02:09:55 +0530

    cards are craeted

--- Frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ Frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,43 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import DashboardLayout from '../../components/layouts/DashboardLayout';
-import { useUserAuth } from '../../hooks/useUserAuth';
-import { useNavigate } from 'react-router-dom';
-import { API_PATHS } from '../../utils/apiPaths';
-import axiosInstance from '../../utils/axiosinstance';
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosinstance";
+
+import { LuWallet, LuCoins } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
+
+import { addThousandsSeparator } from "../../utils/helper";
+import InfoCard from "../../components/Cards/InfoCard";
 
 const Home = () => {
   useUserAuth();
   const navigate = useNavigate();
 
-const [dashboardData, setDashboardData] = useState(null);
-const [loading, setLoading] = useState(false);
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-const fetchDashboardData = async () => {
-  if (loading) return;
+  const fetchDashboardData = async () => {  
+    if (loading) return;
+    setLoading(true);
 
-  setLoading(true);
-
-  try {
-    const response = await axiosInstance.get(
-      `${API_PATHS.DASHBOARD.GET_DATA}`
-    );
-
-    if (response.data) {
-      setDashboardData(response.data);
+    try {
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.GET_DATA);
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
     }
-  } catch (error) {
-    console.log("Something went wrong. Please try again.", error);
-  } finally {
-    setLoading(false);
-  }
-};
+  };
+
   useEffect(() => {
     fetchDashboardData();
-    return() => {};
   }, []);
+
   return (
-    <DashboardLayout activeMenu = "Dashboard">
-      <div className="my-5 mx-auto"></div>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">
+        <div className="flex flex-col gap-6">
+          <InfoCard
+            icon={<LuWallet />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<LuCoins />}
+            label="Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Expense"
+            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+          />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
